9b7beb4913ce1cd53b0fafd4547025175d0d3ec0
Kingsley Solomon <kinxly@gmail.com>
2023-03-09 15:39:44 +0100

    added contact page

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat
+      enim perspiciatis deserunt nulla vero odio ducimus ipsum? Esse delectus,
+      quas repellat beatae vel atque doloribus ut possimus maiores repellendus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+      obcaecati nemo doloribus dignissimos ipsum, perspiciatis nisi totam,
+      aliquam modi similique cupiditate deserunt quidem maxime! Odio, rerum ex
+      nostrum quo tempore illum, illo placeat a quam consequatur pariatur sunt
+      itaque, repudiandae ab? Suscipit harum illo officiis minus magni, alias
+      ipsam omnis iusto debitis, a saepe adipisci aliquam quasi veniam nobis
+      atque similique consequuntur! Velit omnis sequi, similique aliquid
+      voluptatibus nam illo deleniti consectetur sunt, sed nesciunt aliquam
+      beatae blanditiis unde corporis vel? Soluta dolore pariatur quis
+      voluptates nihil voluptate facilis quisquam laudantium, incidunt aliquam
+      ea, unde quia voluptatem quas. Adipisci cumque illo eius aliquam optio aut
+      exercitationem quis sapiente, voluptate neque amet ex quae maxime sunt
+      repellendus fuga? Voluptas assumenda totam officia sed quia facere
+      laboriosam in a, quo dolor dignissimos doloremque? Dolore tenetur esse vel
+      optio ea! Corrupti saepe ipsa tempora voluptatum quae fugit ducimus in,
+      impedit cupiditate nulla vitae fuga minima temporibus obcaecati ut autem
+      voluptas consectetur hic mollitia neque officia accusantium ad totam?
+      Aliquam fugit consequuntur voluptates non, minima eaque. Magni, atque
+      exercitationem quos veniam accusantium laborum libero dolor. Harum
+      reiciendis a sunt cumque optio provident at, autem tempora vitae explicabo
+      unde deserunt obcaecati minima aliquid quis distinctio!
+    </div>
+  );
+};
+
+export default Contact;
